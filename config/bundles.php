e1c94feeb1924e2cfb0943f743f054ee6cf2712b
yayaveli <yabadji2010@gmail.com>
2021-10-17 12:21:39 +0000

    Préparation au déploiement

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
